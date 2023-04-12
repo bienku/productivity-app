0d1d547f69758fb356f19500645b53d04cc1cc70
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-04-12 22:10:39 +0200

    :sparkles: Created register form with yup validation

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,74 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Field, Form, Formik, FormikHelpers } from 'formik';
+import { Box, Button, TextField } from '@mui/material';
+import { registerSchema } from '../schema/account';
+
+const initialValues = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+};
 
 const Register = () => {
-    return <Typography variant="h1">Register</Typography>;
+    const handleSubmit = (values: typeof initialValues, actions: FormikHelpers<typeof initialValues>) => {
+        console.log(values);
+        console.log(actions);
+    };
+
+    return (
+        <Box width="350px" m={8}>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={(values, actions) => handleSubmit(values, actions)}
+                validationSchema={registerSchema}
+            >
+                {({ errors, touched, isValid, dirty }) => (
+                    <Form>
+                        <Field
+                            name="email"
+                            type="email"
+                            label="Email"
+                            as={TextField}
+                            fullWidth
+                            error={errors.email && touched.email}
+                            helperText={Boolean(touched.email) && errors.email}
+                        />
+                        <Box height={14} />
+                        <Field
+                            name="password"
+                            type="password"
+                            label="Password"
+                            as={TextField}
+                            fullWidth
+                            error={errors.password && touched.password}
+                            helperText={Boolean(touched.password) && errors.password}
+                        />
+                        <Box height={14} />
+                        <Field
+                            name="confirmPassword"
+                            type="password"
+                            label="Confirm Password"
+                            as={TextField}
+                            fullWidth
+                            error={errors.confirmPassword && touched.confirmPassword}
+                            helperText={Boolean(touched.confirmPassword) && errors.confirmPassword}
+                        />
+                        <Box height={20} />
+                        <Button
+                            disabled={!isValid || !dirty}
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            fullWidth
+                        >
+                            Sign up
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Box>
+    );
 };
 
 export default Register;
---
+++ src/schema/account.ts
@@ -0,0 +1,18 @@
+import * as yup from 'yup';
+
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
+// min 5 chars, 1 upper, 1 lower, 1 digit
+
+export const registerSchema = yup.object().shape({
+    email: yup.string().email('Please enter a valid email').required('Required!'),
+    password: yup
+        .string()
+        .min(5, 'Password must be at least 5 characters')
+        .max(50, 'Password cannot exceed 50 characters')
+        .matches(passwordRules, 'Please create a stronger password')
+        .required('Required!'),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password')], 'Password must match')
+        .required('Required!'),
+});
