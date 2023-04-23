53070057cfb5135992e44cbe22b3cbac664f0131
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-04-23 19:49:42 +0200

    :sparkles: Centered forms and added onClick functions

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,53 +1,70 @@
 import React from 'react';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, Container, Link, TextField, Typography } from '@mui/material';
 import { Field, Form, Formik, FormikHelpers } from 'formik';
 import { loginSchema } from '../schema/account';
-
-const initialValues = {
-    email: '',
-    password: '',
-};
+import useAuth from '../hooks/useAuth';
+import { Link as RouterLink } from 'react-router-dom';
 
 const Login = () => {
+    const { signIn } = useAuth();
+
+    const initialValues = {
+        email: '',
+        password: '',
+    };
+
     const handleSubmit = (values: typeof initialValues, actions: FormikHelpers<typeof initialValues>) => {
-        console.log(values);
-        console.log(actions);
+        signIn(values.email, values.password);
     };
 
     return (
-        <Box width="350px" m={8}>
-            <Formik
-                initialValues={initialValues}
-                onSubmit={(values, actions) => handleSubmit(values, actions)}
-                validationSchema={loginSchema}
-            >
-                {({ handleBlur }) => (
-                    <Form>
-                        <Field
-                            name="email"
-                            type="email"
-                            label="Email"
-                            as={TextField}
-                            fullWidth
-                            handleBlur={handleBlur}
-                        />
-                        <Box height={14} />
-                        <Field
-                            name="password"
-                            type="password"
-                            label="Password"
-                            as={TextField}
-                            fullWidth
-                            handleBlur={handleBlur}
-                        />
-                        <Box height={20} />
-                        <Button type="submit" variant="contained" color="primary" size="large" fullWidth>
-                            Sign in
-                        </Button>
-                    </Form>
-                )}
-            </Formik>
-        </Box>
+        <Container>
+            <Box display="flex" alignItems="center" justifyContent="center" height="90vh">
+                <Box width="350px" m={8}>
+                    <Typography variant="h3" textAlign="center" mb={3}>
+                        Login
+                    </Typography>
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={(values, actions) => handleSubmit(values, actions)}
+                        validationSchema={loginSchema}
+                    >
+                        {({ handleBlur }) => (
+                            <Form>
+                                <Field
+                                    name="email"
+                                    type="email"
+                                    label="Email"
+                                    as={TextField}
+                                    fullWidth
+                                    onBlur={handleBlur}
+                                />
+                                <Box height={14} />
+                                <Field
+                                    name="password"
+                                    type="password"
+                                    label="Password"
+                                    as={TextField}
+                                    fullWidth
+                                    onBlur={handleBlur}
+                                />
+                                <Box height={20} />
+                                <Button type="submit" variant="contained" color="primary" size="large" fullWidth>
+                                    Sign in
+                                </Button>
+                            </Form>
+                        )}
+                    </Formik>
+
+                    <Typography mt={3} textAlign="center">
+                        Need an account?
+                        <Link component={RouterLink} to="/register" pl={1}>
+                            Sign up
+                        </Link>
+                    </Typography>
+                </Box>
+            </Box>
+        </Container>
     );
 };
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,73 +1,91 @@
 import React from 'react';
 import { Field, Form, Formik, FormikHelpers } from 'formik';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, Container, Link, TextField, Typography } from '@mui/material';
 import { registerSchema } from '../schema/account';
-
-const initialValues = {
-    email: '',
-    password: '',
-    confirmPassword: '',
-};
+import useAuth from '../hooks/useAuth';
+import { Link as RouterLink } from 'react-router-dom';
 
 const Register = () => {
+    const { signUp } = useAuth();
+
+    const initialValues = {
+        email: '',
+        password: '',
+        confirmPassword: '',
+    };
+
     const handleSubmit = (values: typeof initialValues, actions: FormikHelpers<typeof initialValues>) => {
-        console.log(values);
-        console.log(actions);
+        signUp(values.email, values.password);
     };
 
     return (
-        <Box width="350px" m={8}>
-            <Formik
-                initialValues={initialValues}
-                onSubmit={(values, actions) => handleSubmit(values, actions)}
-                validationSchema={registerSchema}
-            >
-                {({ errors, touched, isValid, dirty }) => (
-                    <Form>
-                        <Field
-                            name="email"
-                            type="email"
-                            label="Email"
-                            as={TextField}
-                            fullWidth
-                            error={errors.email && touched.email}
-                            helperText={Boolean(touched.email) && errors.email}
-                        />
-                        <Box height={14} />
-                        <Field
-                            name="password"
-                            type="password"
-                            label="Password"
-                            as={TextField}
-                            fullWidth
-                            error={errors.password && touched.password}
-                            helperText={Boolean(touched.password) && errors.password}
-                        />
-                        <Box height={14} />
-                        <Field
-                            name="confirmPassword"
-                            type="password"
-                            label="Confirm Password"
-                            as={TextField}
-                            fullWidth
-                            error={errors.confirmPassword && touched.confirmPassword}
-                            helperText={Boolean(touched.confirmPassword) && errors.confirmPassword}
-                        />
-                        <Box height={20} />
-                        <Button
-                            disabled={!isValid || !dirty}
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                            size="large"
-                            fullWidth
-                        >
-                            Sign up
-                        </Button>
-                    </Form>
-                )}
-            </Formik>
-        </Box>
+        <Container>
+            <Box display="flex" alignItems="center" justifyContent="center" height="90vh">
+                <Box width="350px" m={8}>
+                    <Typography variant="h3" textAlign="center" mb={3}>
+                        Register
+                    </Typography>
+
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={(values, actions) => handleSubmit(values, actions)}
+                        validationSchema={registerSchema}
+                    >
+                        {({ errors, touched, isValid, dirty }) => (
+                            <Form>
+                                <Field
+                                    name="email"
+                                    type="email"
+                                    label="Email"
+                                    as={TextField}
+                                    fullWidth
+                                    error={errors.email && touched.email}
+                                    helperText={Boolean(touched.email) && errors.email}
+                                />
+                                <Box height={14} />
+                                <Field
+                                    name="password"
+                                    type="password"
+                                    label="Password"
+                                    as={TextField}
+                                    fullWidth
+                                    error={errors.password && touched.password}
+                                    helperText={Boolean(touched.password) && errors.password}
+                                />
+                                <Box height={14} />
+                                <Field
+                                    name="confirmPassword"
+                                    type="password"
+                                    label="Confirm Password"
+                                    as={TextField}
+                                    fullWidth
+                                    error={errors.confirmPassword && touched.confirmPassword}
+                                    helperText={Boolean(touched.confirmPassword) && errors.confirmPassword}
+                                />
+                                <Box height={20} />
+                                <Button
+                                    disabled={!isValid || !dirty}
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    size="large"
+                                    fullWidth
+                                >
+                                    Sign up
+                                </Button>
+                            </Form>
+                        )}
+                    </Formik>
+
+                    <Typography mt={3} textAlign="center">
+                        Already have an account?
+                        <Link component={RouterLink} to="/login" pl={1}>
+                            Log in
+                        </Link>
+                    </Typography>
+                </Box>
+            </Box>
+        </Container>
     );
 };
 
